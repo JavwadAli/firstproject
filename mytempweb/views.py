f735f5c229fd0cecd09e4eb17192377ef2c0e7af
JavwadAli <59450591+JavwadAli@users.noreply.github.com>
2020-01-09 22:09:22 +0530

    Update views.py

--- mytempweb/views.py
+++ mytempweb/views.py
@@ -4,7 +4,7 @@ def navigation(request):
     s = '<head><style>#chakra{color:blue}#block{text-align:center;border:2px solid black;margin:100px;}#amz{color:orange;text-decoration:none;}#google{color:#9e9e9e;text-decoration:none;}#gmail{color:green;text-decoration:none;}</style></head>' \
         '<div id="block"><h1><a href="https://amazon.com" id="amz">AMAZON</h1>' \
         '<h1><a href="https://google.com" id="google">GO<span id = "chakra">O</span>GLE</h1>' \
-        '<h1><a href="https://gmail.com" id="gmail">GMAIL</h1></div>'
+        '<h1><a href="https://gmail.com" id="gmail">Gmail</h1></div>'
 
     return HttpResponse(s)
 def nav(request):
